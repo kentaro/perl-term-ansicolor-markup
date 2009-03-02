1516f67ad82d4427bb7a62972817a9d3e8b2cf9d
Kentaro Kuribayashi <kentarok@gmail.com>
2009-03-02 23:47:58 +0900

    Checking in changes prior to tagging of version 0.05.  Changelog diff is:

--- lib/Term/ANSIColor/Markup.pm
+++ lib/Term/ANSIColor/Markup.pm
@@ -5,7 +5,7 @@ use warnings;
 use Term::ANSIColor::Markup::Parser;
 use base qw(Class::Accessor::Lvalue::Fast);
 
-our $VERSION = '0.04';
+our $VERSION = '0.05';
 
 __PACKAGE__->mk_accessors(qw(parser text));
 
